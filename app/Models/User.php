0f94d116eb50e185bcb439faa4089a71150070b4
Tim <timleenights@gmail.com>
2017-05-13 14:40:49 +0000

    Finish user signup

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,29 +11,35 @@ use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
 
 class User extends Model implements AuthenticatableContract,
-                                    AuthorizableContract,
-                                    CanResetPasswordContract
+                  AuthorizableContract,
+                  CanResetPasswordContract
 {
-    use Authenticatable, Authorizable, CanResetPassword;
+  use Authenticatable, Authorizable, CanResetPassword;
 
-    /**
-     * The database table used by the model.
-     *
-     * @var string
-     */
-    protected $table = 'users';
+  /**
+  * The database table used by the model.
+  *
+  * @var string
+  */
+  protected $table = 'users';
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = ['name', 'email', 'password'];
+  /**
+  * The attributes that are mass assignable.
+  *
+  * @var array
+  */
+  protected $fillable = ['name', 'email', 'password'];
 
-    /**
-     * The attributes excluded from the model's JSON form.
-     *
-     * @var array
-     */
-    protected $hidden = ['password', 'remember_token'];
+  /**
+  * The attributes excluded from the model's JSON form.
+  *
+  * @var array
+  */
+  protected $hidden = ['password', 'remember_token'];
+
+  public function gravatar($size = '100')
+  {
+    $hash = md5(strtolower(trim($this->attributes['email'])));
+    return "http://www.gravatar.com/avatar/$hash?s=$size";
+  }
 }
